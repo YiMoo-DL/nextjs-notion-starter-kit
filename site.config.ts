23c9fe1aaa058aa95d3de1107c3ee064dc1df9d0
Yimoo <74238423+YiMoo-DL@users.noreply.github.com>
2022-11-06 17:45:39 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Yimoo-694b2fd2c5fa4a39be7cd1635fa2b5b7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
